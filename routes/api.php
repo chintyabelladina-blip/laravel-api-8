0a222a5cb1ed39f77d748c4683958165acce473e
dinachintya44-coder <dina.chintya44@smk.belajar.id>
2026-08-06 08:22:02 +0700

    project data

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request
 $request) {
 return $request->user();
 });
-Route::get('/product',[ProductController::class,'index'])->name('product');
+Route::get('/product',[ProductController::class,'index'])->name('product.index');
+Route::post('/product',[ProductController::class,'store'])->name('product.store');
+Route::put('/product/{product}', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/product/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
